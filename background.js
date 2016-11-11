fb3aa7b91422703410e6b30604eb7c85dd2b48fe
Shiaupiau <stu43005@gmail.com>
2016-11-11 20:41:15 +0800

    v0.1.2 - 新增支援Chrome同步功能
    token現在會同步在所有已登入相同Google帳號的裝置中!

--- background.js
+++ background.js
@@ -1,9 +1,21 @@
-if (localStorage.getItem('token') == "undefined") {
-	localStorage.removeItem('token');
+if (localStorage.getItem('token') !== null && localStorage.getItem('noteStoreUrl') !== null) {
+	chrome.storage.sync.set({
+		"token": localStorage.getItem('token'),
+		"noteStoreUrl": localStorage.getItem('noteStoreUrl')
+	}, function() {
+		console.log("Upgraded extension!\nNow token are syncing with every devices if login same Google account.");
+		localStorage.removeItem('token');
+		localStorage.removeItem('noteStoreUrl');
+	});
 }
-if (localStorage.getItem('noteStoreUrl') == "undefined") {
-	localStorage.removeItem('noteStoreUrl');
-}
+
+chrome.storage.sync.get(["token", "noteStoreUrl"], function(items) {
+	if (items.token == "undefined" || items.noteStoreUrl == "undefined") {
+		chrome.storage.sync.remove(["token", "noteStoreUrl"], function() {
+			console.error("token error, cleared.");
+		});
+	}
+});
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 	if (request.type == 'require_verifier') {
@@ -15,13 +27,19 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 		localStorage.removeItem("oauth_token_secret");
 		localStorage.removeItem("oauth_verifier");
 
-		localStorage.setItem("token", request.data.oauth_token);
-		localStorage.setItem("noteStoreUrl", request.data.edam_noteStoreUrl);
+		chrome.storage.sync.set({
+			"token": request.data.oauth_token,
+			"noteStoreUrl": request.data.edam_noteStoreUrl
+		}, function() {
+			console.log("token saved.");
+			chrome.storage.sync.get(["token", "noteStoreUrl"], function(items) {
+				sendResponse(items);
+			});
+		});
 	}
-	if (request.type == 'set_edam' || request.type == 'get_edam') {
-		sendResponse({
-			token: localStorage.getItem('token'),
-			noteStoreUrl: localStorage.getItem('noteStoreUrl'),
+	if (request.type == 'get_edam') {
+		chrome.storage.sync.get(["token", "noteStoreUrl"], function(items) {
+			sendResponse(items);
 		});
 	}
 	if (request.type == "set_default_notebook") {
